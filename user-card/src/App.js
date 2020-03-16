0755eea1708926073f3d1875afe3d94f548e26eb
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-03-16 11:59:34 +0800

    building out followers functionality

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -14,18 +14,17 @@ class App extends React.Component {
     axios
       .get("https://api.github.com/users/yenszen")
       .then(res => {
-        console.log("App.js: CDM: ", res);
+        console.log("CDM axios GET: initial ", res);
         this.setState({
-          users: [res.data] // res.data is an object, so had to be put into an array to gain access to map function!
+          users: [...this.state.users, res.data] // res.data is an object, so had to be put into an array to gain access to map function!
         });
-        console.log("App.js: CDM: this.state.users", typeof this.state.users);
       })
       .catch(err => console.log(err));
 
     axios
       .get("https://api.github.com/users/yenszen/followers")
       .then(res => {
-        console.log("App.js: CDM: followers: ", res);
+        console.log("CDM axios GET: followers: ", res);
         this.setState({
           users: [...this.state.users, res.data]
         });
@@ -35,29 +34,31 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <h1>React Github User Card</h1>
-        {/*  */}
-        {this.state.users.map(user => {
-          return (
-            <div className="card" key={user.id}>
-              <img src={user.avatar_url} />
-              <div className="card-info">
-                <h3 className="name">{user.name}</h3>
-                <p className="username">GitHub username: {user.login}</p>
-                <p>
-                  <a href={`user.html_url`} target="_blank">
-                    GitHub profile
-                  </a>
-                </p>
-                <p>Followers: {user.followers}</p>
-                <p>Following: {user.following}</p>
-                <p>Bio: {user.bio}</p>
+      console.log("latest users data", this.state.users),
+      (
+        <div className="App">
+          <h1>React Github User Card</h1>
+          {this.state.users.map(user => {
+            return (
+              <div className="card" key={user.id}>
+                <img src={user.avatar_url} />
+                <div className="card-info">
+                  <h3 className="name">{user.login}</h3>
+                  <p className="username">GitHub username: {user.login}</p>
+                  <p>
+                    <a href={`${user.html_url}`} target="_blank">
+                      GitHub profile
+                    </a>
+                  </p>
+                  <p>Bio: {user.bio}</p>
+                  <p>Followers: {user.followers}</p>
+                  <p>Following: {user.following}</p>
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      )
     );
   }
 }
